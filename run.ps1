a1e41f4fdc992291af88887c25129d78860a0521
amd64fox <62529699+amd64fox@users.noreply.github.com>
2023-10-03 03:49:11 +0300

    Test-Internet Connection function removed

--- run.ps1
+++ run.ps1
@@ -1,69 +1,3 @@
-function Test-InternetConnection {
-    function Connect {
-        param (
-            [string]$Domain
-        )
-
-        try {
-            $tcpClient80 = New-Object System.Net.Sockets.TcpClient
-            $tcpClient443 = New-Object System.Net.Sockets.TcpClient
-
-            $tcpClient80.Connect($Domain, 80)
-            $tcpClient443.Connect($Domain, 443)
-
-            if ($tcpClient80.Connected -or $tcpClient443.Connected) {
-                return $true
-            }
-
-            $tcpClient80.Close()
-            $tcpClient443.Close()
-        }
-        catch {}
-
-        try {
-            $ping = Test-Connection -ComputerName $Domain -Count 1 -Quiet -ErrorAction Stop
-            if ($ping) {
-                return $true
-            }
-        }
-        catch {}
-
-        return $false
-    }
-
-    $domains = "www.google.com", "www.bing.com", "www.baidu.com", "yandex.com"
-    $maxRetryAttempts = 3
-    $retryPauseSeconds = 3
-    $retryCount = 0
-
-    do {
-        $internetAvailable = $false
-        foreach ($domain in $domains) {
-            $result = Connect -Domain $domain
-            if ($result) {
-                $internetAvailable = $true
-                break
-            }
-        }
-
-        if ($internetAvailable) {
-            break
-        }
-
-        $retryCount++
-        if ($retryCount -lt $maxRetryAttempts) {
-            Write-Host "Retesting in $retryPauseSeconds seconds..."
-            Start-Sleep -Seconds $retryPauseSeconds
-        }
-        else {
-            Write-Host
-            Write-Warning "After $maxRetryAttempts attempts, no internet connection was detected. Exiting script."
-            pause
-            exit
-        }
-    } while ($retryCount -lt $maxRetryAttempts)
-}
-
 function Send-Cutt {
 
     $Uri = "https://cutt.ly/RwbSmBPM"
@@ -121,7 +55,5 @@ function Run-SpotX {
 }
 
 [Net.ServicePointManager]::SecurityProtocol = 3072
-
-Test-InternetConnection
 
 Run-SpotX -params $args
